db69dd1ed6d27c1a6d3cdd9757fb71bcb6443e61
Maryna Lakhno <marynalakhno@Marynas-MacBook-Air.local>
2023-06-18 00:28:05 +0200

    Added sample pdf and tested extractor

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+from pypdf import PdfReader
+
+
+def extract_all_text():
+    reader = PdfReader("./samples/bristol_2021-22.pdf")
+    for page in reader.pages:
+        print(page.extract_text())
+        print()
+   
 
 def run():
+   OPENAI_API_KEY = "***REMOVED***"
+
    pass
 
 if __name__ == "__main__":
-   run()
+   extract_all_text()
